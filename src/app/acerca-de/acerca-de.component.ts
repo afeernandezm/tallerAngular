2d0bdc61c8d11792a96b22894c92a2abfff8d240
afeernandezm <92725027+afeernandezm@users.noreply.github.com>
2023-03-28 12:54:07 +0200

    Add files via upload

--- src/app/acerca-de/acerca-de.component.ts
+++ src/app/acerca-de/acerca-de.component.ts
@@ -1,54 +1,44 @@
 import { EntradaService } from './../shared/services/entrada.service';
 import { Observable } from 'rxjs';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Piezas } from './../piezas';
-import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
   styleUrls: ['./acerca-de.component.css'],
 })
 export class AcercaDeComponent implements OnInit {
-  public listadoEntradas: Piezas[];
-  idPiezaSeleccionada: string;
-  piezaSeleccionada: Piezas;
-  idPieza: string = '';
-  nombrePieza: string = '';
-  precioPieza: number = 0;
+  public listadoEntradas: Piezas[] = [];
+  idPiezaSeleccionada = '';
+  piezaSeleccionada: Piezas = {
+    _id: '',
+    nombre: '',
+    precio: 0,
+  };
+  idPieza = '';
+  nombrePieza = '';
+  precioPieza = 0;
   @Input()
-  public piezas: Piezas;
-  EntradaService: any;
-  showAlert: boolean;
+  piezas: Piezas = {
+    _id: '',
+    nombre: '',
+    precio: 0,
+  };
+  showAlert = false;
 
   constructor(
     private http: HttpClient,
     private cdRef: ChangeDetectorRef,
     private entradaService: EntradaService
-  ) {
-    this.piezas = {
-      _id: '',
-      nombre: '',
-      precio: 0,
-    };
-    this.idPiezaSeleccionada = this.piezas._id;
-    this.piezaSeleccionada = {
-      _id: '',
-      nombre: '',
-      precio: 0,
-    };
-    this.listadoEntradas = [];
-    this.showAlert = false;
-  }
+  ) {}
 
   ngOnInit(): void {
     // Carga de datos desde la API
-    this.http
-      .get<Piezas[]>('https://tallerbackend.onrender.com/api/piezas')
-      .subscribe((data) => {
-        this.listadoEntradas = data;
-      });
+    this.obtenerPiezas();
   }
+
   async obtenerPiezas() {
     try {
       const response = await this.http
@@ -60,6 +50,7 @@ export class AcercaDeComponent implements OnInit {
       // Aquí puedes mostrar un mensaje de error al usuario
     }
   }
+
   seleccionarPieza(piezaId: string) {
     this.piezaSeleccionada = this.listadoEntradas.find(
       (p: Piezas) => p._id === piezaId.trim()
@@ -69,7 +60,6 @@ export class AcercaDeComponent implements OnInit {
       precio: 0,
     };
     this.idPiezaSeleccionada = piezaId.trim();
-
     console.log(this.piezaSeleccionada);
     console.log(this.idPiezaSeleccionada);
   }
@@ -89,17 +79,4 @@ export class AcercaDeComponent implements OnInit {
       // Aquí puedes mostrar un mensaje de error al usuario
     }
   }
-
-  /*   modificar(idPieza: string) {
-    console.log('idPieza: ' + idPieza);
-    const piezaModificada = {
-      nombre: this.nombrePieza,
-      precio: this.precioPieza,
-    };
-
-    this.piezasService.modificarPieza(this.idPieza, piezaModificada).subscribe(
-      (data) => console.log(data),
-      (error) => console.error(error)
-    );
-  } */
 }
